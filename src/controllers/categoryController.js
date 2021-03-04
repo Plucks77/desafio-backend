0fdfc8a6979104cb646b0262166ba29adf9d5a8a
Pedro Lucas <pedro_leoti@hotmail.com>
2021-03-04 19:43:51 -0300

    Update - Agora a query de todas categorias retorna a quantidade de produtos que possui

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -3,8 +3,11 @@ const knex = require("../database/connection");
 class CategoryController {
   async all(req, res) {
     try {
-      const categories = await knex.select("*").from("category");
-      return res.json(categories);
+      const categories = await knex.raw(
+        "SELECT c.name , (SELECT Count(*)  FROM product b Where b.category_id = c.id ) as amount FROM  category c"
+      );
+
+      return res.json(categories[0]);
     } catch (error) {
       return res.status(500).json({
         error: error.message,
