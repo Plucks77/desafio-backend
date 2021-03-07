2b1a90819f267fe299bc9603b4a2348d6593e703
Pedro Lucas <pedro_leoti@hotmail.com>
2021-03-07 08:53:55 -0300

    Updates

--- src/server.js
+++ src/server.js
@@ -8,5 +8,5 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333);
-process.env.PORT;
+app.listen(3333);
+console.log("Server running on 3333 ...");
